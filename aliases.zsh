20edea9d710317826ba3b1327b3c1a13327fd7da
Kurt Rush <krush@Kurts-MacBook-Pro.local>
2019-01-16 06:55:17 -0500

    Inital Installation

--- aliases.zsh
+++ aliases.zsh
@@ -2,28 +2,15 @@
 alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
 alias reloadcli="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
-alias ll="$(brew --prefix coreutils)/libexec/gnubin/ls -ahlF --color --group-directories-first"
 weather() { curl -4 wttr.in/${1:-antwerp} }
-alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
 alias library="cd $HOME/Library"
-alias sites="cd $HOME/Sites"
 
-# Laravel
-alias a="php artisan"
-alias ams="php artisan migrate:fresh --seed"
-
-# Vagrant
-alias v="vagrant global-status"
-alias vup="vagrant up"
-alias vhalt="vagrant halt"
-alias vssh="vagrant ssh"
-alias vreload="vagrant reload"
-alias vrebuild="vagrant destroy --force && vagrant up"
+# alias sites="cd $HOME/Sites"
 
 # Docker
 alias docker-composer="docker-compose"
@@ -33,13 +20,3 @@ alias docker-composer="docker-compose"
 #dbuild() { docker build -t=$1 .; }
 #dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
 
-# Git
-alias commit="git add . && git commit -m"
-alias gcommit="git add . && git commit"
-alias wip="commit wip"
-alias gst="git status"
-alias gb="git branch"
-alias gc="git checkout"
-alias gd="git diff"
-alias gl="git log --oneline --decorate --color"
-alias gnuke="git clean -df && git reset --hard"
